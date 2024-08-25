8365da00ccec9e62a36af753b19020ea3d6b10fe
rcl <24314861+rc-l@users.noreply.github.com>
2024-08-25 22:46:45 +0200

    add some clarification

--- scim2/tests/conftest.py
+++ scim2/tests/conftest.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pytest
 
+# Ensure the scim2 package can be imported
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
 
