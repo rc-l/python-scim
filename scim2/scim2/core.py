171b73e1440c5c13f74ef4113785a8fa7ca844da
rcl <24314861+rc-l@users.noreply.github.com>
2024-08-26 21:14:03 +0200

    Added base schema with common attributes

--- scim2/scim2/core.py
+++ scim2/scim2/core.py
@@ -1,10 +1,24 @@
-import json
-from copy import deepcopy, copy
+from .base import Attribute, ResourceBase, ComplexBase
+from .datatypes import *
 
-from .base import Attribute, Base
+class MetaData(ComplexBase):
+    """Metadata for a resource"""
+    
+    resourceType = Attribute(String, mutability="readOnly", caseExact=True)
+    created = Attribute(DateTime)
+    lastModified = Attribute(DateTime)
+    location = Attribute(String)
+    version = Attribute(String)
 
 
+class BaseSchema(ResourceBase):
+    """Base class for schema objects"""
+    
+    id = Attribute(String, required=True)
+    externalId = Attribute(String)
+    meta = Attribute(MetaData)
 
-# class User(ResourceBase):
-#     username = Singular()
-#     emails = MultiValue()
+
+class User(BaseSchema):
+    username = Attribute(String)
+    emails = Attribute(String, multivalued=True)
---
+++ scim2/tests/test_schema.py
@@ -0,0 +1,49 @@
+from datetime import datetime, timezone
+
+from scim2.core import BaseSchema
+
+class TestBaseSchema:
+
+    def test_from_dict(self):
+        """Test creating a BaseSchema object from a dictionary"""
+        data = {
+            "id": "test", 
+            "externalId": "test2",
+            "meta": {
+                "resourceType": "BaseObject",
+                "created": "2010-01-23T04:56:22Z",
+                "lastModified": "2010-01-23T04:56:22Z",
+                "location": "https://example.com/Users/test",
+                "version": 'W/"3694e05e9dff594"'
+            }
+        }
+        user = BaseSchema(data)
+        assert user.id.value == "test"
+        assert user.externalId.value == "test2"
+        assert user.meta.value.resourceType.value == "BaseObject"
+        assert user.meta.value.created.value == datetime(2010, 1, 23, 4, 56, 22, tzinfo=timezone.utc)
+        assert user.meta.value.lastModified.value == datetime(2010, 1, 23, 4, 56, 22, tzinfo=timezone.utc)
+        assert user.meta.value.location.value == "https://example.com/Users/test"
+        assert user.meta.value.version.value == 'W/"3694e05e9dff594"'
+
+    def test_to_dict(self):
+        """Test converting a BaseSchema object to a dictionary"""
+        user = BaseSchema()
+        user.id.value = "foo"
+        user.externalId.value = "bar"
+        user.meta.value.resourceType.value = "BaseObject"
+        user.meta.value.created.value = datetime(2012, 1, 23, 4, 56, 22, tzinfo=timezone.utc)
+        user.meta.value.lastModified.value = datetime(2013, 1, 23, 4, 56, 22, tzinfo=timezone.utc)
+        user.meta.value.location.value = "https://example.com/Users/test"
+        user.meta.value.version.value = 'W/"3694e05e9eee594"'
+        assert user.dict() == {
+            "id": "foo",
+            "externalId": "bar",
+            "meta": {
+                "resourceType": "BaseObject",
+                "created": "2012-01-23T04:56:22+00:00",
+                "lastModified": "2013-01-23T04:56:22+00:00",
+                "location": "https://example.com/Users/test",
+                "version": 'W/"3694e05e9eee594"'
+            }
+        }
